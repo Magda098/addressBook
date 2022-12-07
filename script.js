ee681257dd2579163291ead70876b5adb279ca37
Magda098 <magjac22@gmail.com>
2022-12-07 08:33:40 +0300

    adds code to collect user input from the html form

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-//contact constructor
+//contact constructor / business logic
 function Contact(first, last) {
     this.firstName = first; 
     this.lastName = last;
 }
 //new contact object
-var ada = new Contact("Ada", "Lovelace");
+/*var ada = new Contact("Ada", "Lovelace");
 var me =new Contact("mag","mutheu")
-console.log(me.firstName);
+console.log(me.firstName);*/
+
+  // user interface logic
+  $(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+  
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+  
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+  
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+  
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+    });
+  });
