f76e64b362c487ab0bcc9b1919feae21851d4e95
Magda098 <magjac22@gmail.com>
2022-12-07 08:19:18 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//contact constructor
+function Contact(first, last) {
+    this.firstName = first; 
+    this.lastName = last;
+}
+//new contact object
+var ada = new Contact("Ada", "Lovelace");
+var me =new Contact("mag","mutheu")
+console.log(me.firstName);
